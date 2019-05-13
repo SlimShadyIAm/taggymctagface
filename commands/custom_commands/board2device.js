359ad003d7776736476d41854f180c1b0f953591
Aamir Farooq <aamir@farooq.xyz>
2019-05-13 11:49:26 +0200

    Add board2device command

---
+++ commands/custom_commands/board2device.js
@@ -0,0 +1,49 @@
+const {
+    Command
+} = require('discord.js-commando');
+const {
+    MessageEmbed
+} = require('discord.js');
+const SQLite = require("better-sqlite3");
+const sql = SQLite('./commands.sqlite');
+const board2device = require("../../boardnamedevices.json")
+
+module.exports = class Board2DeviceCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'board2device',
+            group: 'custom commands',
+            memberName: 'board2device',
+            description: 'Find the device name for a board',
+            guildOnly: true,
+            examples: ['board2device coral'],
+            args: [{
+                    key: 'board',
+                    prompt: 'What is the board name?',
+                    type: 'string'
+                }
+            ]
+        });
+    }
+
+    run(msg, { board }) {
+        if (board in board2device) {
+            const embed = new MessageEmbed()
+                .setColor(7506394)
+                .setDescription(`Board **${board}** belongs to the following device(s): **${board2device[board]}**`);
+            return msg.channel.send({ embed });
+        } else {
+            return sendErrorResponse(msg, `Board **${board}** does not exist! Please enter a valid board name`)
+        }
+
+        function sendErrorResponse(msg, text) {
+            msg.channel.send({
+                embed: {
+                    color: 12663868,
+                    title: "An error occured!",
+                    description: text
+                }
+            })
+        }
+    }
+}
