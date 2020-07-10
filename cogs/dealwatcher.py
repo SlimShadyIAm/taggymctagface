4e5fd10d26c0cadc455059e867a65075ba351a69
Aamir Farooq <aamir@farooq.xyz>
2020-07-10 20:16:07 +0200

    Finally got this working time to test

--- cogs/dealwatcher.py
+++ cogs/dealwatcher.py
@@ -10,7 +10,7 @@ bott = None
 class DealWatcher(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        feeds = [
+        self.feeds = [
             {
                 'feed': "https://www.aboutchromebooks.com/feed/",
                 'name': "AboutChromebooks.com",
@@ -44,80 +44,68 @@ class DealWatcher(commands.Cog):
             }
         ]
 
-        for feed in feeds:
-            thread = threading.Thread(target=watcher, args=(feed, bot,), daemon=True)
-            thread.start()
+        self.watcher.start()
 
-        # loop.run_forever()
     def cog_unload(self):
-        # loop = asyncio.get_event_loop()
-        # loop.close()
+        self.watcher.cancel()
         print("STOPPING...")
 
-# the watcher thread
-@tasks
-def watcher(feed, bot):
-    print("Starting watcher...")   
-    pp = pprint.PrettyPrinter(indent=4)
+    # the watcher thread
+    @tasks.loop(seconds=10)
+    async def watcher(self):
+        print("Starting watcher...")   
+        pp = pprint.PrettyPrinter(indent=4)
+        for feed in self.feeds:
+            if feed['good_feed'] is True:
+                await self.good_feed(feed)
+            else:
+                await self.bad_feed(feed)
 
-    while True:
-        if feed['good_feed'] is True:
-            good_feed(feed, bot)
-        else:
-            bad_feed(feed, bot)
-        time.sleep(60)
+    # feed watcher for feeds with proper etag support
+    async def good_feed(self, feed):
+        data = feedparser.parse(feed["feed"])
+        data = feedparser.parse(feed["feed"], modified=feed["prev_data"].modified)
+        if (data.status != 304):
+            for post in data.entries:
+                print(f'NEW GOOD ENTRY: {post.title} {post.link}')
+            self.check_new_entries(feed, data.entries)
+        feed["prev_data"] = data
 
-# feed watcher for feeds with proper etag support
-def good_feed(feed, bot):
-    data = feedparser.parse(feed["feed"])
-    data = feedparser.parse(feed["feed"], modified=feed["prev_data"].modified)
-    if (data.status != 304):
-        for post in data.entries:
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            loop.run_until_complete(push_update(post, feed, bot))
-            print(f'NEW GOOD ENTRY: {post.title} {post.link}')
-        check_new_entries(feed, data.entries)
-    feed["prev_data"] = data
-
-# improper etag support
-def bad_feed(feed, bot):
-    data = feedparser.parse(feed["feed"])
-    for post in data.entries:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(push_update(post, feed, bot))
-    prev_ids = [something["id"] for something in feed["prev_data"].entries]
-    new_ids = [something["id"] for something in data.entries if something["id"] not in prev_ids]
-    
-    new_posts = [post for post in data.entries if post.id in new_ids]
-    if (len(new_posts) > 0):
-        for post in new_posts:
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            loop.run_until_complete(push_update(post, feed, bot))
-            print(f'NEW BAD ENTRY: {post.title} {post.link}')
-        check_new_entries(feed, new_posts)
+    # improper etag support
+    async def bad_feed(self, feed):
+        data = feedparser.parse(feed["feed"])
+        prev_ids = [something["id"] for something in feed["prev_data"].entries]
+        new_ids = [something["id"] for something in data.entries if something["id"] not in prev_ids]
+        
+        new_posts = [post for post in data.entries if post.id in new_ids]
+        if (len(new_posts) > 0):
+            for post in new_posts:
+                print(f'NEW BAD ENTRY: {post.title} {post.link}')
+            self.check_new_entries(feed, new_posts)
 
 
-def check_new_entries(feed, entries):
-    pp = pprint.PrettyPrinter(indent=4)
-    # pp.pprint(entries)
-    for entry in entries:
-        post_tags = [tag.term.lower() for tag in entry.tags]
-        if len(feed["requiredFilters"]) != 0:
-            match = [tag for tag in feed["filters"] if tag in post_tags]
-            match_required = [tag for tag in feed["requiredFilters"] if tag in post_tags]
-            if (len(match) > 0 and len(match_required) > 0):
-                print(f'MATCH FOUND {entry.title}, {entry.link}, {entry.tags}')
-        else:
-            match = [tag for tag in feed["filters"] if tag in post_tags]
-            if (len(match) > 0):
-                print(f'MATCH FOUND {entry.title}, {entry.link}, {entry.tags}')
+    async def check_new_entries(self, feed, entries):
+        pp = pprint.PrettyPrinter(indent=4)
+        # pp.pprint(entries)
+        for entry in entries:
+            post_tags = [tag.term.lower() for tag in entry.tags]
+            if len(feed["requiredFilters"]) != 0:
+                match = [tag for tag in feed["filters"] if tag in post_tags]
+                match_required = [tag for tag in feed["requiredFilters"] if tag in post_tags]
+                if (len(match) > 0 and len(match_required) > 0):
+                    print(f'MATCH FOUND {entry.title}, {entry.link}, {entry.tags}')
+                    await self.push_update(entry, feed)
+            else:
+                match = [tag for tag in feed["filters"] if tag in post_tags]
+                if (len(match) > 0):
+                    print(f'MATCH FOUND {entry.title}, {entry.link}, {entry.tags}')
+                    await self.push_update(entry, feed)
 
-async def push_update(post, feed, bot):
-    channel = bot.get_guild(525250440212774912).get_channel(621704381053534257)
-    await (channel.send(f'NEW ENTRY: {post.title} {post.link} {feed["name"]}'))
+
+    async def push_update(self, post, feed):
+        channel = self.bot.get_guild(525250440212774912).get_channel(621704381053534257)
+        await (channel.send(f'New deal was posted!\n{post.title}\n{post.link}'))
+      
 def setup(bot):
     dw = DealWatcher(bot)
     bot.add_cog(dw)
