c886647e864da7a37dbfc982a1e638be450c91da
Aamir Farooq <aamir@farooq.xyz>
2020-07-11 15:07:18 +0200

    Work on list

--- cogs/list.py
+++ cogs/list.py
@@ -6,24 +6,13 @@ import random
 from os.path import dirname, abspath
 import os
 
-class Test:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-
-data = [
-    Test(key=key, value=value)
-    for key in ['test', 'other', 'okay']
-    for value in range(20)
-]
-
 class Source(menus.GroupByPageSource):
     async def format_page(self, menu, entry):
-        # joined = '\n'.join(f'{i}. <Test value={v.value}>' 
         embed = Embed(title=f'Commands: Page {menu.current_page +1}/{self.get_max_pages()}')
+        print (entry)
         for i, v in enumerate(entry.items, start=1):
-            embed.add_field(name=i, value=v.value)
-        # return f'**{entry.key}**\n{joined}\nPage {menu.current_page + 1}/{self.get_max_pages()}'
+            res = f'\n**Response**:{v[5]}' if v[6] == 'true' else ''
+            embed.add_field(name=f'${v[3]}', value=f'**ID**:{v[0]}\n**Supports arguments**:{v[6]}{res}\n**Creator**:<@{v[2]}>\n**Number of uses**:{v[4]}')
         return embed
 
 class Add(commands.Cog):
@@ -32,7 +21,16 @@ class Add(commands.Cog):
     
     @commands.command(name='list')
     async def list(self, ctx):
-        pages = menus.MenuPages(source=Source(data, key=lambda t: t.key, per_page=12), clear_reactions_after=True)
+        BASE_DIR = dirname(dirname(abspath(__file__)))
+        db_path = os.path.join(BASE_DIR, "commands.sqlite")
+        conn = sqlite3.connect(db_path)
+        c = conn.cursor()
+        c.execute("SELECT * FROM commands WHERE server_id = ? ORDER BY command_name", (ctx.guild.id,))
+
+        data = c.fetchall()
+        conn.close()
+
+        pages = menus.MenuPages(source=Source(data, key=lambda t: 1, per_page=6), clear_reactions_after=True)
         await pages.start(ctx)
 
 def setup(bot):
