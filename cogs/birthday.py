4a1a66e32460367d72dc64d90bab34f2f385ade4
Aamir Farooq <aamir@farooq.xyz>
2020-07-11 21:12:11 +0200

    typo

--- cogs/birthday.py
+++ cogs/birthday.py
@@ -19,11 +19,11 @@ class Birthday(commands.Cog):
             await ctx.send(embed=Embed(title="An error occured!", color=Color(value=0xEB4634), description="Please supply a member, i.e `$birthday @SlimShadyIAm#9999`"))
             return
 
-        await member.add_roles(discord.utils.get(ctx.guild.roles, name="Birthday boi"))
+        await member.add_roles(discord.utils.get(ctx.guild.roles, name="birthday boi"))
         await ctx.send(embed=Embed(title="Done!", color=Color(value=0x37b83b), description=f'Gave <@{member.id}> the birthday role. We\'ll let them know and remove it in 24 hours.'))
         await member.send(embed=Embed(title="Happy birthday!  🥳", color=Color(value=0xebde34), description=f'{ctx.author.name} gave you the birthday role. We\'ll remove it in 24 hours.'))
         await asyncio.sleep(86400)
-        await member.remove_roles(discord.utils.get(ctx.guild.roles, name="Birthday boi"))
+        await member.remove_roles(discord.utils.get(ctx.guild.roles, name="birthday boi"))
         await ctx.author.send(embed=Embed(title="Done!", color=Color(value=0x37b83b), description=f'Removed {member.name}\'s birthday role.'))
         await member.send(embed=Embed(title="Party's over.", color=Color(value=0x37b83b), description='Removed your birthday role.'))
 def setup(bot):
