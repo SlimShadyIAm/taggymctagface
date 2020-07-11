d6f3d44610f822e2f056fbbf4f68c2ef46b78de0
Aamir Farooq <aamir@farooq.xyz>
2020-07-11 19:37:56 +0200

    Finished d2b

--- cogs/device2board.py
+++ cogs/device2board.py
@@ -1,6 +1,6 @@
 import discord
 from discord import Embed, Color
-from discord.ext import commands
+from discord.ext import commands, menus
 import aiohttp, asyncio, json
 import re
 
@@ -32,9 +32,31 @@ class DeviceToBoard(commands.Cog):
         devices = json.loads(response)
 
         search_results = [ (device["Codename"], device["Brand names"]) for device in devices if 'Brand names' in device and search_term in device['Brand names'].lower() ]
-                
-        await ctx.send(embed=Embed(title="An error occured!", color=Color(value=0xEB4634), description="A board with that name was not found!"))
+        if len(search_results) == 0:        
+            await ctx.send(embed=Embed(title="An error occured!", color=Color(value=0xEB4634), description="A board with that name was not found!"))
+        else:
+            pages = NewMenuPages(source=Source(search_results, key=lambda t: 1, per_page=12), clear_reactions_after=True)
+            await pages.start(ctx)
 
+class Source(menus.GroupByPageSource):
+    async def format_page(self, menu, entry):
+        embed = Embed(title=f'Search results: Page {menu.current_page +1}/{self.get_max_pages()}')
+        for v in entry.items:
+            embed.add_field(name=v[0], value=(v[1][:75] + '...') if len(v[1]) > 75 else v[1])
+        return embed
+
+class NewMenuPages(menus.MenuPages):
+    async def update(self, payload):
+        if self._can_remove_reactions:
+            if payload.event_type == 'REACTION_ADD':
+                await self.bot.http.remove_reaction(
+                    payload.channel_id, payload.message_id,
+                    discord.Message._emoji_reaction(payload.emoji), payload.member.id
+                )
+            elif payload.event_type == 'REACTION_REMOVE':
+                return
+        await super().update(payload)
+        
 
 def setup(bot):
     bot.add_cog(DeviceToBoard(bot))
