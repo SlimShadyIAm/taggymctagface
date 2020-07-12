da4c76f0f9a202fe68d2240a34f2decfc7fda5e2
Aamir Farooq <aamir@farooq.xyz>
2020-07-12 20:11:31 +0200

    Oop

--- cogs/crosblog.py
+++ cogs/crosblog.py
@@ -1,8 +1,11 @@
-import discord
-from discord import Embed, Color
-from discord.ext import commands
 import asyncio
+import os
+
+import discord
 import feedparser
+from discord import Color, Embed
+from discord.ext import commands
+
 
 class CrosBlog(commands.Cog):
     def __init__(self, bot):
